a428a3cae988f293c9f7af0526f43600a5d35cd6
Anurag Sawant <anuragsawant@duck.com>
2025-11-20 21:41:19 +0530

    Refactor Badge and Info components to simplify structure; remove unused props and state management

--- app/(with-layout)/info/Badge.tsx
+++ app/(with-layout)/info/Badge.tsx
@@ -1,29 +1,12 @@
-"use client";
-
 import Image from "next/image";
 
-interface BadgeProps {
-  id: number;
-  fallen: boolean;
-  fadeIn: boolean;
-  onClick: (id: number) => void;
-}
-
-const Badge = ({ id, fallen, fadeIn, onClick }: BadgeProps) => {
+const Badge = () => {
   return (
     <Image
-      key={`${id}-${fallen}`} // Add this key
       src="/badge.png"
       width={300}
       height={300}
-      onClick={() => onClick(id)}
-      className={`md:w-80 w-24 cursor-pointer animate__animated transition-all duration-300 ${
-        fallen
-          ? "animate__hinge invert-[0.3]"
-          : fadeIn
-          ? "animate__fadeIn"
-          : "hover:invert-[0.2]"
-      }`}
+      className={`md:w-80 w-24 cursor-pointer animate__animated transition-all duration-300`}
       alt="Badge"
     />
   );
